7ed46eda2baffc8f490c60323ddc106cf0edb3ee
Rahim10020 <rahialighi@gmail.com>
2025-07-11 16:32:37 +0000

    utilisation de bun

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-npm install
-chmod +x ./node_modules/.bin/tailwindcss
-npm run build:css
+bun install
+bun run build:css
 python manage.py collectstatic --no-input
 python manage.py migrate 
