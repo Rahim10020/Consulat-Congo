a3bf90ea443b74f8344f188fe19e59f7340e2bc7
Rahim10020 <rahialighi@gmail.com>
2025-07-11 16:27:17 +0000

    corrections

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 npm install
-chmod +x node_modules/.bin/tailwindcss
+chmod +x ./node_modules/.bin/tailwindcss
 npm run build:css
 python manage.py collectstatic --no-input
 python manage.py migrate 
